87d9a3db5acb9064b51e7ddc55dac3c09508bbe2
s4kr4 <s4kr4m4@gmail.com>
2017-06-26 00:17:51 +0900

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'users/index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :user
+end
