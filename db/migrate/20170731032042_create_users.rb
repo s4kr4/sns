0c225ed69cd0708189d5aa040856344e8c12f0df
s4kr4 <s4kr4m4@gmail.com>
2017-07-31 05:24:35 +0200

    Rebuild User model

--- db/migrate/20170731032042_create_users.rb
+++ db/migrate/20170731032042_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :screen_name
-      t.string :user_name
+      t.string :name
+      t.string :email
 
       t.timestamps
     end
