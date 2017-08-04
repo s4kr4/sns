29a7dcb1971c54ab7e7aaae738d5d68aeae0a14b
s4kr4 <s4kr4m4@gmail.com>
2017-08-04 06:10:13 +0200

    Add user validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
 
   describe "User model" do
     before do
-      @user = User.new(name: "testuser", email: "testuser@test.com")
+      @user = User.new(name: "testuser", email: "testuser@example.com")
     end
 
     context "Valid user model" do
@@ -30,6 +30,29 @@ RSpec.describe User, type: :model do
       it "email should not be too long" do
         @user.email = "a" * 247 + "@test.com"
         expect(@user).not_to be_valid
+      end
+
+      it "email validation should accept valid addresses" do
+        valid_address = %w[testuser@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+        valid_address.each do |valid_address|
+          @user.email = valid_address
+          expect(@user).to be_valid, "#{valid_address.inspect} should be valid"
+        end
+      end
+
+      it "email validation should reject invalid addresses" do
+        invalid_address = %w[testuser@example,com user_at_foo.bar.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+        invalid_address.each do |invalid_address|
+          @user.email = invalid_address
+          expect(@user).not_to be_valid, "#{invalid_address.inspect} should be invalid"
+        end
+      end
+
+      it "email addresses should be unique" do
+        duplicate_user = @user.dup
+        duplicate_user.email = @user.email.upcase
+        @user.save
+        expect(duplicate_user).not_to be_valid
       end
     end
   end
